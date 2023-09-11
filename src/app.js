8b8396859f10f1542fafb63c3b0c3b5c5c24313d
גל היקרי <galhikri305@gmail.com>
2023-09-11 22:11:48 +0300

    deployment

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import routes from "./routes";
 import { config } from "./config";
+import path from "path";
 import "./services/mongoose";
 
 const app = express();
@@ -12,5 +13,10 @@ const PORT = config.server.port || 3005;
 app.use(cors());
 app.use(bodyParser.json());
 app.use(routes);
+
+app.use("/", express.static(path.join(__dirname, "../../client/build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../../client/build", "index.html"));
+});
 
 app.listen(PORT, () => console.log(`App listening at port ${PORT}`));
--- src/dal/models/user/index.js
+++ src/dal/models/user/index.js
@@ -1,15 +1,16 @@
 import mongoose from "mongoose";
-import uniqueValidator from "mongoose-unique-validator";
 
-const userSchema = new mongoose.Schema({
-    email: {type: String},
-    password: {type: String},
-    first_name: {type: String},
-    last_name: {type: String},
-}, {
-    timestamps: true
-})
+const userSchema = new mongoose.Schema(
+  {
+    email: { type: String },
+    password: { type: String },
+    first_name: { type: String },
+    last_name: { type: String },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-// userSchema.plugin(uniqueValidator)
-const userModel = mongoose.model("users", userSchema)
+const userModel = mongoose.model("users", userSchema);
 export default userModel;
